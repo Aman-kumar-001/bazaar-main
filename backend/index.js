face760f537cdcc012fb920ccaab00f8d5c9b94f
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2024-07-30 22:27:20 +0530

    user added backend confirm

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ const dotenv = require('dotenv')
 dotenv.config();
 
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URL).
+mongoose.connect('mongodb://localhost:27017/Bazzar').
     then(() => console.log('DB connection successfull!')).catch((err) => { console.log(err) });
 
 app.use(cors());
